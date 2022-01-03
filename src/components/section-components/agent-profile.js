729c560553ffe42d28f8a400bea662aa999d5575
Zahra Shahid <zashahid45@gmail.com>
2022-01-03 22:07:22 +0500

    errors fixed on Teams page and an agent-profile component added

---
+++ src/components/section-components/agent-profile.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import parse from 'html-react-parser';
+
+const AgentProfile = ({
+  name,
+  role,
+  facebook,
+  linkedIn,
+  instagram,
+  twitter
+}) => {
+
+    return (
+        
+    <div className="col-lg-4 col-md-6">
+    <div className="single-agent-wrap style-2 text-center">
+      <div className="thumb">
+        <img src={process.env.PUBLIC_URL+'/' +"assets/img/agent/7.png" }alt="img" />
+      </div> 
+
+      <div className="details">
+        <h4>{name}</h4>
+        <h6>{role}</h6>
+        <ul className="social-area style-2">
+          <li><a href={facebook}><i className="fab fa-facebook-f" aria-hidden="true" /></a></li>
+          <li><a href={linkedIn}><i className="fab fa-linkedin-in" aria-hidden="true" /></a></li>
+          <li><a href={instagram}><i className="fab fa-instagram" aria-hidden="true" /></a></li>
+          <li><a href={twitter}><i className="fab fa-twitter" aria-hidden="true" /></a></li>
+        </ul>
+      </div>   
+
+    </div>
+  </div>
+
+    );
+};
+
+
+export default AgentProfile;
