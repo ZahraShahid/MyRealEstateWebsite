b377a3f0e2b50c1f8bcb643614d18f576cc4ed3b
Mariam Ayaz <bsef18m512@pucit.edu.pk>
2022-01-12 20:40:58 +0500

    gallery made dynamic

---
+++ Backend/models/Gallery.models.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const GallerySchema = new Schema(
+    {
+        image: {type: String, required: true},
+               
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Gallery = mongoose.model('Gallery', GallerySchema);
+
+module.exports = Gallery;
--- src/components/section-components/gallery.js
+++ src/components/section-components/gallery.js
@@ -1,78 +1,56 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import parse from 'html-react-parser'; 
+import ImagesProfile from './images-profile';
+import axios from 'axios';
+
 class Gallery extends Component {
 
+  constructor(props)
+	  {
+		  super(props);
+
+      this.state = {
+        Images: []
+		  }
+    }
+
+  componentDidMount() {
+
+     axios.get('http://localhost:5000/Gallery/')
+     .then(response => {
+        if (response.data) {
+           this.setState({      
+             Images: response.data
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
     render() {
 
-        let publicUrl = process.env.PUBLIC_URL+'/'
+        console.log(this.state.images);
 
     return <div className="agent-area text-center pd-top-118 pd-bottom-90">
             <div className="container">
               <div className="row">
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
-                <div className="col-lg-4 col-md-6">
-                <div className="single-product-wrap style-2">
-			            <div className="thumb">
-			              <img src={publicUrl+"assets/img/project/1.png"} alt="img" />
-			            </div>        
-			           </div>
-                </div>
+
+              {
+                    this.state.Images.map((currentImage) => { 
+                      return (
+                      <ImagesProfile 
+                      key={Math.random()}
+                      image={currentImage.image}
+                      />
+                      ); 
+                    })       
+                  }
+               
+               
+                       
+                
               </div>
             </div>
           </div>
