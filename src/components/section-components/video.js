f37846d753b4a2c5e2c4f24c71e47af3b3ba2829
HammadAhmed28 <chhahmed082@gmail.com>
2022-01-13 14:13:56 +0500

    video

--- src/components/section-components/video.js
+++ src/components/section-components/video.js
@@ -3,18 +3,41 @@ import { Link } from 'react-router-dom';
 import parse from 'html-react-parser';
  class Video extends Component {
 
+  
+
+    constructor(props)
+	{
+	  super(props);
+
+      this.state = {
+        Video: []
+	  }
+    }
+
+  componentDidMount() {
+
+     axios.get('http://localhost:5000/Video/')
+     .then(response => {
+        if (response.data) {
+           this.setState({      
+             Video: response.data
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+
     render() {
-
-        // let publicUrl = process.env.PUBLIC_URL+'/'
-
     return <div className="video-area bg-overlay">
 			  <div className="container">
 			    <div className="row justify-content-center">
 			      <div className="col-xl-8 col-lg-10">
 			        <div className="section-title text-center">
-			          <h2>The Mordan House Video</h2>
-			          <p>Lorem ipsum dolor  amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-			          <a className="play-btn" href="https://www.youtube.com/embed/Wimkqo8gDZ0" data-effect="mfp-zoom-in"><i className="fa fa-play" /></a>
+			          <h2>{this.state.Video.VideoTitle}</h2>
+			          <p>{this.state.Video.VideoDescription}</p>
+			          <a className="play-btn" href={this.state.Video.VideoLink} data-effect="mfp-zoom-in"><i className="fa fa-play" /></a>
 			        </div>
 			      </div>
 			    </div>
